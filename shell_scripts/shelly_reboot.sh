f0799d292445d34e69c7f84752da9b1778208d84
Giorgio Ravera <giorgio.ravera@gmail.com>
2019-12-16 12:00:30 +0100

    Added Shelly Sensors, added Shelly Reboot script, minor additional changes

---
+++ shell_scripts/shelly_reboot.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+HASS_HOME="/var/lib/homeassistant/.homeassistant"
+USER=$(awk '/shelly_user/ { print  $2 }' $HASS_HOME/secrets.yaml)
+PASSWORD=$(awk '/shelly_password/ { print  $2 }' $HASS_HOME/secrets.yaml)
+SHELLY_ID=(
+	shelly1.giorgioravera.it
+	shelly2.giorgioravera.it
+	shelly3.giorgioravera.it
+	shelly4.giorgioravera.it
+	shelly5.giorgioravera.it
+	shelly6.giorgioravera.it
+	shelly7.giorgioravera.it
+	shelly8.giorgioravera.it
+	shelly9.giorgioravera.it
+	shellybulb.giorgioravera.it
+	shellyem.giorgioravera.it
+	shellyplugs.giorgioravera.it
+)
+
+echo "Reboot Shelly Devices"
+
+for i in "${SHELLY_ID[@]}"
+do
+	ALIVE=$(ping -c 1 $i |grep ttl)
+	if [ ! -z "$ALIVE" ]; then
+		STATUS=$(curl -s --user $USER:$PASSWORD http://$i/reboot | sed -e "s/{"ok"}://g")
+		echo $STATUS
+	fi
+done
+
+echo "Done"
