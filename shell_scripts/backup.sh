046a53c714803747227d431d481302d1229b6068
Giorgio Ravera <giorgio.ravera@gmail.com>
2020-10-10 23:54:05 +0200

    Fixed backup script.

--- shell_scripts/backup.sh
+++ shell_scripts/backup.sh
@@ -4,6 +4,6 @@ FILENAME=homeassistant_$(date +"%Y%m%d").tar.bz2
 
 docker container stop homeassistant
 cd ../../
-tar cfvj homeassistant/backups/$FILENAME --exclude='backups' --exclude='tts/*' --exclude='home-assistant.log' --exclude='home-assistant_v2.db' homeassistant
-#docker container start homeassistant
+tar cfvj homeassistant/backups/$FILENAME --exclude='backups' --exclude='tts/*' --exclude='home-assistant.log' --exclude='home-assistant_v2.db*' homeassistant
+docker container start homeassistant
 cd -
